2b77f2ddc0b8ab7b941abf824326daf3b1aa7591
Nikhil-VIP <nikhilchauha007@gmail.com>
2026-01-19 21:16:56 +0530

    tendays css

--- Weather_app/src/tendays.jsx
+++ Weather_app/src/tendays.jsx
@@ -1,8 +1,17 @@
-
+import "./tendays.css"
 const Tendays = () => {
   return (
     <>
-      <h1>10 Days Weather Forecast Coming Soon!</h1>
+      <h1>10 Days Weather Forecast</h1>
+      <h5>"location name"</h5>
+      <div id="daysconatiner">
+        <div id="days">
+          <h5>"day"</h5>
+          <div id="pic">"weatherpic"</div>
+          <div id="condition">"condition"</div>
+          <div id="temp">"temprature"</div>
+        </div>
+      </div>
     </>
   )
 }
